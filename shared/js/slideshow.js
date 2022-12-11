ef4f5365156ab6397ce14d580fabd5701e8ffa47
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-12-11 14:05:58 -0700

    started with part 3, done with start the show, thank God

--- shared/js/slideshow.js
+++ shared/js/slideshow.js
@@ -1,13 +1,19 @@
 var images;
+var index = 0;
+var border = 10;
+// console.log("here");
 
-var canvas = document.getElementById('slideshow').getContext('2d');
+
+var canvas = document.getElementById('slideshow');
+var ctx = canvas.getContext('2d');
 
 function start(){
 
-  document.getElementById("start_button").addEventListener("click", startSlideShow, false);
-  document.getElementById("stop_button").addEventListener("click", stopSlideShow, false);
-  document.getElementById("back_button").addEventListener("click", changeDirection, false);
-  document.getElementById("random_button").addEventListener("click", changeSequence, false);
+  document.getElementById("start_button").addEventListener("click", start_show, false);
+//   document.getElementById("stop_button").addEventListener("click", stopSlideShow, false);
+//   document.getElementById("back_button").addEventListener("click", changeDirection, false);
+//   document.getElementById("random_button").addEventListener("click", changeSequence, false);
+  console.log("here");
 
   load_images();
 }
@@ -15,13 +21,41 @@ function start(){
 function load_images(){
   var xhr = new XMLHttpRequest();
 
-  xhr.open('GET', '../shared/images/', true);
+  xhr.open('GET', '../shared/images/pics.json', true);
 
   xhr.onload = function(){
     if(this.status == 200) {
-      image = JSON.parse(this.responseText);
+      images = JSON.parse(this.responseText);
     }
   }
 
   xhr.send();
 }
+
+function start_show(){
+    document.getElementById('start_button').disabled = true;
+    load_image(images[index].address);
+}
+
+function load_image(address)
+{
+  var image = new Image();
+  console.log("in load image function");
+  image.onload = function(){ 
+    // canvas.width = image.naturalWidth+border;
+    // canvas.height = image.naturalHeight+border;
+    ctx.drawImage(image, 0, 0);
+    // ctx.strokeStyle = '#7d8c99';
+    // ctx.lineWidth = 10;
+    // ctx.strokeRect(border/2, border/2, canvas.width-border, canvas.height-border);
+  };
+
+//   console.log(address);
+
+
+  image.src = address;
+
+
+}
+
+window.addEventListener("load", start, false);
